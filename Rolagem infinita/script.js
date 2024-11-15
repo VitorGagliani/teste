214a57d3b9d34d0ffecefd474d9123ef019247ff
Vitor Gagliani <93485462+VitorGagliani@users.noreply.github.com>
2024-11-14 22:58:02 -0300

    Add files via upload

---
+++ Rolagem infinita/script.js
@@ -0,0 +1,49 @@
+const container = document.getElementById("container");
+const loader = document.getElementById("loader");
+let currentPage = 1;
+
+function imagem() {
+  return `https://picsum.photos/300/300?random=${Math.random()}`;
+}
+
+// Função para carregar conteúdo
+function loadContent() {
+  loader.style.display = "block"; // Exibe o loader
+
+  // Simula uma requisição
+  setTimeout(() => {
+    const newContent = `
+      <p class"loader">Págiona N° ${currentPage}</p>
+       <div class="id">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    <img src="${imagem()}" alt="">
+    
+
+        
+   </div>
+      `;
+    container.innerHTML += newContent;
+    currentPage++;
+    loader.style.display = "none"; // Esconde o loader
+  }, 1000); // Simula um atraso de 1 segundo
+}
+
+// Carrega o conteúdo inicial
+loadContent();
+
+// Detecta a rolagem
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    loadContent();
+  }
+});
